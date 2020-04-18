b24a43bc5b8050bd6c137b2a112774a718b06024
Tee-15 <temitopedml@gmail.com>
2020-04-18 08:58:51 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function myFunction() {
+    var x = document.getElementById("mywrapper-link")
+    if (x.style.display === "block") {
+        x.style.display ="none"
+    } else {
+        x.style.display ="block"
+    }
+}
+
+
